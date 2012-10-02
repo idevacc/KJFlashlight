a8f9f9bd5f62ce0ee8a6d8949465947667d20882
Kristopher Johnson <kris@kristopherjohnson.net>
2012-10-02 08:18:03 -0400

    Initial Commit

---
+++ KJFlashlight/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KJFlashlight
+//
+//  Created by Kristopher Johnson on 10/2/12.
+//  Copyright (c) 2012 Kristopher Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ KJFlashlight/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KJFlashlight
+//
+//  Created by Kristopher Johnson on 10/2/12.
+//  Copyright (c) 2012 Kristopher Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
